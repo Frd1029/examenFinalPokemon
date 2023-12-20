b5a37f640076eb0995d1709be461425a44b55877
Frd1029 <freduar1029@gmail.com>
2023-12-20 10:42:56 -0500

    Configuración de Cors y SpringSecurity. Así como implementacion de algunos metodos en controllers.

--- src/main/java/com/example/examen/controllers/entrenadorController.java
+++ src/main/java/com/example/examen/controllers/entrenadorController.java
@@ -3,10 +3,17 @@ package com.example.examen.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.examen.repositorios.entrenadorRepositorio;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.examen.entity.Entrenador;
 
 @RestController
@@ -20,8 +27,19 @@ public class entrenadorController {
     }
 
     @GetMapping
-    public List<Entrenador> getEntrenadores(){
+    public List<Entrenador> getEntrenadores() {
         return entrenadorRepositorio.findAll();
     }
 
+    @PostMapping("/login")
+    public String getUuidByEmail(@RequestBody String emailPeticion) throws JsonMappingException, JsonProcessingException{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(emailPeticion);
+
+            if (jsonNode.has("uuid")) { 
+                String email = jsonNode.get("email").asText();
+                return entrenadorRepositorio.findByEmail(email).get(0).getUuid();
+            } 
+            return null;
+    }
 }
